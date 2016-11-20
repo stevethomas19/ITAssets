b4a3bd663a61254780be7fcd013ddf6c66cb61fa
Steve Thomas <gamgi1@hotmail.com>
2016-11-19 17:41:52 -0700

    change id in it_asset to be 10 digit unique

--- app/models/it_asset.rb
+++ app/models/it_asset.rb
@@ -1,2 +1,14 @@
+require 'securerandom'
+
 class ItAsset < ActiveRecord::Base
+  before_create :randomize_id
+
+  private
+
+  #this method makes the unique identifier 10 alphanumeric characters
+  def randomize_id
+    begin
+      self.id = SecureRandom.hex 5
+    end while ItAsset.where(id: self.id).exists?
+  end
 end
